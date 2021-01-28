21e1dd478e53c9ecffdfe4437a8b2132f4156fff
tosakla <78131882+tosakla@users.noreply.github.com>
2021-01-28 13:16:02 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const ALLOWED_GROUPS = []
 export const BOT_TOKEN = '997333589:AAH4Bo-ER925sv2E1oeYlkha9EE09rHa7nU'
-export const ROBOT_NAME = '@_bot'
+export const ROBOT_NAME = '@tosaka_bot'
